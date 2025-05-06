a681752e4016976c38cdb02fb595254ecd7bb486
Vinnie Falco <vinnie.falco@gmail.com>
2025-05-06 07:58:01 -0700

    [WIP] asio compatibility refactor

---
+++ test/asio.cpp
@@ -0,0 +1,30 @@
+//
+// Copyright (c) 2025 Vinnie Falco (vinnie.falco@gmail.com)
+//
+// Distributed under the Boost Software License, Version 1.0. (See accompanying
+// file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+//
+// Official repository: https://github.com/CPPAlliance/buffers
+//
+
+#include <boost/asio/buffer.hpp>
+
+#include "test_helpers.hpp"
+
+namespace boost {
+namespace buffers {
+
+struct asio_test
+{
+    void
+    run()
+    {
+    }
+};
+
+TEST_SUITE(
+    asio_test,
+    "boost.buffers.asio");
+
+} // buffers
+} // boost
